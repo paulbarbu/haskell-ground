303ec8f4379ff0b122bf385ceae198fa0718b6c6
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-12-15 11:04:59 +0200

    properly showing the source-destination rod for moving the disks

--- HanoiTowers.hs
+++ HanoiTowers.hs
@@ -1,18 +1,15 @@
-h :: Int -> [String]
-h n 
- | n > 1 = (ab (n-1)) ++ ["A-C"] ++ (bc (n-1))
- | otherwise = ["n>1 please"]
-
-ab :: Int -> [String]
-ab 1 = ["A-B"]
-ab n = h n
-
-bc :: Int -> [String]
-bc 1 = ["B-C"]
-bc n = h n
+{-| Solve hanoi for n given disks by moving them from source
+ - through helper on the destination rod
+ -}
+h :: Int -> String -> String -> String -> [String]
+h n source helper dest
+ | n == 1 = [source ++ "-" ++ dest]
+ | otherwise = h (n-1) source dest helper ++
+    [source ++ "-" ++ dest] ++
+    h (n-1) helper source dest
 
 main :: IO()
 main = do
     n <- getLine
     let (discs,_) = (reads n) !! 0
-    putStrLn $ unwords (h discs)
+    putStrLn $ unwords (h discs "A" "B" "C")
