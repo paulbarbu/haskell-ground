d1672e7640bd2608e7908f38b68abd7365b7b560
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-12-31 14:21:03 +0200

    added the trandLine function for linear simple regression

--- DrawLine.hs
+++ DrawLine.hs
@@ -1,5 +1,7 @@
 module DrawLine where
 
+import Data.List (foldl')
+
 data Point = Point Float Float deriving (Show)
 
 getRange :: Int -> Int -> [Int]
@@ -23,7 +25,9 @@ getLinePoints f@(Point fx fy) l@(Point lx ly)
     -- vertical line
     | fx == lx = [(Point fx (fromIntegral y)) | y <- getRange (round fy) (round ly)]
     | otherwise =
+        -- generate points by mapping on the "longer side" of the line
         if m > 1.0 || m < -1.0 then
+            -- the rise if higher than the run
             map (\y ->
                     (Point
                         ((1/m) * ((fromIntegral y) - fy) + fx)
@@ -41,3 +45,29 @@ getLinePoints f@(Point fx fy) l@(Point lx ly)
                 (getRange (round fx) (round lx))
         where
             m = getSlope f l
+
+{-|
+ - Given two lists of points, first on the X-axis, second on the Y-axis return
+ - the coefficients of the first grade equation that approximates the data set
+ -
+ - In other words get a linear trend for the data set
+ -
+ - The elements of the returned pair (a,b) map like this to the linear equation:
+ - f(x) = ax + b
+ -
+ - This function is using the ordinary least squares method to determine the
+ - line equation
+ - See: https://en.wikipedia.org/wiki/Ordinary_least_squares
+ -
+ - TODO: length xs == 0, xSquareSum == 0 (xs == [0..0]), xSquareSum*(length xs) - xSum^2 == 0
+ -}
+trendLine :: (Num a, Fractional a) => [a] -> [a]-> (a, a)
+trendLine xs ys = (a, b)
+    where
+        xSum = foldl' (+) 0 xs
+        ySum = foldl' (+) 0 ys
+        xSquaredSum = foldl' (+) 0 $ map (^2) xs
+        xySum = foldl' (+) 0 $ zipWith (*) xs ys
+
+        b = ((xSquaredSum*ySum) - (xSum*xySum))/((xSquaredSum * fromIntegral (length xs)) - xSum^2)
+        a = (xySum - xSum*b)/xSquaredSum
