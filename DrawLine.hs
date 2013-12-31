7b1610e21a169503a7a329b3b8a98d1ef05607f3
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-12-31 11:14:49 +0200

    added some docs and renamed the function drawLine to getLinePoints

--- DrawLine.hs
+++ DrawLine.hs
@@ -6,12 +6,21 @@ getRange :: Int -> Int -> [Int]
 getRange a b
     | a <= b = [a..b]
     | otherwise = [b..a]
-
+{-|
+ - Get the slope of a line described by the two points received as arguments
+ -}
 getSlope :: Point -> Point -> Float
 getSlope (Point x1 y1) (Point x2 y2) = (y2-y1)/(x2-x1)
 
-drawLine :: Point -> Point -> [Point]
-drawLine f@(Point fx fy) l@(Point lx ly)
+{-|
+ - Get a list of points where pixels should be filled to draw a line
+ - (Inspired from Bresenham's line algorithm)
+ - Takes two Point arguments representing the end points of the line, the points
+ - will be located on a line between these two points
+ -}
+getLinePoints :: Point -> Point -> [Point]
+getLinePoints f@(Point fx fy) l@(Point lx ly)
+    -- vertical line
     | fx == lx = [(Point fx (fromIntegral y)) | y <- getRange (round fy) (round ly)]
     | otherwise =
         if m > 1.0 || m < -1.0 then
@@ -26,9 +35,9 @@ drawLine f@(Point fx fy) l@(Point lx ly)
             map (\x ->
                     (Point
                         (fromIntegral x)
-                        (m * (fromIntegral x - fx) + fy)
+                        (m * ((fromIntegral x) - fx) + fy)
                     )
                 )
                 (getRange (round fx) (round lx))
         where
-            m= getSlope f l
+            m = getSlope f l
